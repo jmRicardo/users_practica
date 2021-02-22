4d8ec87198934680783e35311ff535613ef5b215
jmRicardo <51313148+jmRicardo@users.noreply.github.com>
2021-02-22 14:18:15 -0300

    iniciando setup para repo de usuarios

---
+++ src/main/java/com/jmr/practica/users/users_practica/UsersPracticaApplication.java
@@ -0,0 +1,13 @@
+package com.jmr.practica.users.users_practica;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UsersPracticaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UsersPracticaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/jmr/practica/users/users_practica/UsersPracticaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jmr.practica.users.users_practica;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UsersPracticaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
