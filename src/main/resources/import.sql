5cfd1460f71e849a31cbd6b589eb205267f4b9ce
jmRicardo <51313148+jmRicardo@users.noreply.github.com>
2021-02-22 14:36:46 -0300

    añadida mas configuración al servicio

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,9 +1,9 @@
-INSERT INTO usuarios (username, password, enabled, nombre, apellido, email) VALUES ('andres','$2a$10$ykhXmCAam5FUEF9GN.4Z8OwwWJidvMii6VFYe77cmS2X6oF6p4W86',true, 'Andres', 'Guzman','profesor@bolsadeideas.com');
-INSERT INTO usuarios (username, password, enabled, nombre, apellido, email) VALUES ('admin','$2a$10$qGyDfZLBB.SgLv7GCP3uZe3oM38fVtr58T1iZ1LNOvO61loNUAAaK',true, 'John', 'Doe','jhon.doe@bolsadeideas.com');
+INSERT INTO users (username, password, enabled, firstName, lastName, email) VALUES ('juan','$2a$10$ykhXmCAam5FUEF9GN.4Z8OwwWJidvMii6VFYe77cmS2X6oF6p4W86',true, 'Juan Manuel', 'Ricardo','juanmanricardo@gmail.com');
+INSERT INTO users (username, password, enabled, firstName, lastName, email) VALUES ('admin','$2a$10$qGyDfZLBB.SgLv7GCP3uZe3oM38fVtr58T1iZ1LNOvO61loNUAAaK',true, 'John', 'Doe','jhon.doe@gmail.com');
 
-INSERT INTO roles (nombre) VALUES ('ROLE_USER');
-INSERT INTO roles (nombre) VALUES ('ROLE_ADMIN');
+INSERT INTO roles (name) VALUES ('ROLE_USER');
+INSERT INTO roles (name) VALUES ('ROLE_ADMIN');
 
-INSERT INTO usuarios_roles (usuario_id, role_id) VALUES (1, 1);
-INSERT INTO usuarios_roles (usuario_id, role_id) VALUES (2, 2);
-INSERT INTO usuarios_roles (usuario_id, role_id) VALUES (2, 1);
+INSERT INTO users_with_roles (user_id, role_id) VALUES (1, 1);
+INSERT INTO users_with_roles (user_id, role_id) VALUES (2, 2);
+INSERT INTO users_with_roles (user_id, role_id) VALUES (2, 1);
